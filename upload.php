4282daba2e478cc4519a20eb00559d39e2685d10
Rikka Foxx <67561247+RikkaFoxx@users.noreply.github.com>
2020-07-27 10:53:54 -0400

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: index.php");
+    exit;
+}
+?>
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check == false) {
+    $uploadOk = 1;
+  } else {
+    $uploadOk = 0;
+  }
+}
+
+// Allow certain file formats
+if($imageFileType != "xlsx") {
+  echo "Sorry, XLSX Files are allowed.";
+  $uploadOk = 0;
+}
+
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
