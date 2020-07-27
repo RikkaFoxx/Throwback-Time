33f218dcab06a25f2cfb7bf9587ca09e2bfb078c
Rikka Foxx <67561247+RikkaFoxx@users.noreply.github.com>
2020-07-27 11:45:03 -0400

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SRV', 'localhost');
-define('DB_PASSWD', "Management2018");
-define('DB_USER', 'DaviesJ');
-define('DB_NAME', 'timekeepusers');
+define('DB_SRV', 'REDACTED');
+define('DB_PASSWD', "REDACTED");
+define('DB_USER', 'REDACTED');
+define('DB_NAME', 'REDACTED');
 
 $connection = mysqli_connect(DB_SRV, DB_USER, DB_PASSWD, DB_NAME);
 
